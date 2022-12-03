490c1a24aa671bcadfb034f51511a9eb6dc8b281
brayden <brayden367@gmail.com>
2022-12-02 20:12:39 -0500

    Added functionality to ccontrol wiz lights and be velocity reactive

--- midiToWLED.py
+++ midiToWLED.py
@@ -8,26 +8,43 @@ from __future__ import print_function
 
 import logging
 import sys
+import threading
 import time
 import json
+import pywizlight
+import asyncio
 
 from rtmidi.midiutil import open_midiinput
+del pywizlight.wizlight.__del__
+
+# Check if file exists
+config = []
+with open("config.json", "r") as jsonfile:
+    config = json.load(jsonfile)
+    print("Read successful.\n")
+
 
 # Values
-BAUD = 921600
-START_MIDI = 100
-END_MIDI = 28
-NUM_LEDS = 144
-PORT = 'COM3'
+try:
+    BAUD = config['baud']
+    START_MIDI = config['midiStart']
+    END_MIDI = config['midiEnd']
+    REV_LEDS = (END_MIDI - START_MIDI) < 0
+    NUM_LEDS = config['numLeds']
+    PORT = config['comPort']
+    midiPort = config['midiDevice']
+    WIZLIGHTS_IPS = config['wizLights']
+    LIGHTS = [None] * len(WIZLIGHTS_IPS)
+    RGB = config['RGB']
+    MAX_LIGHT_NUM_KEYS = 10
+except KeyError as e:
+    print("ERROR: Missing config item: " + str(e.args[0]))
 
-# COLOR
-RGB = [255, 20, 20]
-VELOCITY_CURVE = 30
-
-
+# Logging
 log = logging.getLogger('midiin_poll')
 logging.basicConfig(level=logging.DEBUG)
 
+# Serial
 import serial
 ser = serial.Serial()
 ser.baudrate = BAUD
@@ -37,27 +54,29 @@ ser.bytesize=8
 time.sleep(1)
 ser.open()
 
-# Prompts user for MIDI input port, unless a valid port number or name
-# is given as the first argument on the command line.
-# API backend defaults to ALSA on Linux.
-port = sys.argv[1] if len(sys.argv) > 1 else None
-
+# Functions
 def mapRange(value, inMin, inMax, outMin, outMax):
     return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
 
 def getLed(value):
-    return int(mapRange(value, START_MIDI, END_MIDI, 1, NUM_LEDS))
+    #return int(mapRange(value, START_MIDI, END_MIDI, 1, NUM_LEDS))
+    ret = NUM_LEDS - (value - END_MIDI) * 2
+    if( value > NUM_LEDS / 2):
+        return ret+1
+    if( ret > 0 ):
+        return ret
+    return 0
 
 def getVelocityRGB(value):
     newRGB = [0] * 3
     for i in range(len(RGB)):
-        newRGB[i] = int(mapRange(value, 0, 127 - VELOCITY_CURVE, 0, 255))
+        newRGB[i] = int((int(mapRange(value, 0, 127, 100, 255)) / 255) * RGB[i])
     return newRGB
 
 def sendNoteOn(note, velocity):
     if ((note >= START_MIDI) and (note <= END_MIDI)) or ((note >= END_MIDI) and (note <= START_MIDI)):
         led = getLed(note)
-        data = {"seg":{"i":[led-1, RGB, NUM_LEDS-led]}}
+        data = {"seg":{"i":[led-1, getVelocityRGB(velocity), NUM_LEDS-led]}}
         data = json.dumps(data)
         ser.write(data.encode('ascii'))
         print(json.loads(data))
@@ -76,22 +95,58 @@ def sendNoteOff(note):
     else:
         print("Value out of range: " + str(note))
 
+async def updateLight(light, rgbVal, brightness):
+    if(rgbVal == [0,0,0]):
+        await light.turn_off()
+    else:
+        await light.turn_on(pywizlight.PilotBuilder(rgbww = (rgbVal[0], rgbVal[1], rgbVal[2], 0, 0), brightness=brightness))
+    return
+
+async def saveStates(lights):
+    for i in range(len(lights)):
+        light = lights[i]
+        state = await light.updateState()
+        lights[i] = (light, state)
+
+async def updateLightStates(lights):
+    for i in range(len(lights)):
+        light = lights[i]
+        print(repr(light[1]))
+        await light[0].turn_on(pywizlight.PilotBuilder(
+            #rgb = (light[1].get_rgb() if light[1].get_rgb()[0] else [0,0,0]), 
+            brightness=light[1].get_brightness(),
+            #warm_white=light[1].get_warm_white(), 
+            #cold_white=light[1].get_cold_white(),
+            rgbww=light[1].get_rgbww(),
+            colortemp=light[1].get_colortemp(),
+            ratio=light[1].get_ratio()
+        ))
+
+# Open Port
 try:
-    midiin, port_name = open_midiinput(port)
+    midiin, port_name = open_midiinput(midiPort)
 except (EOFError, KeyboardInterrupt):
     sys.exit()
 
-# print("Setting up with color RGB(" + str(RGB[0]) + ',' + str(RGB[1]) + ',' + str(RGB[2]) + ")...")
-# ser.write(chr((255)).encode('latin1'))
-# time.sleep(0.01)
-# ser.write(chr(RGB[0]).encode('latin1'))
-# time.sleep(0.01)
-# ser.write(chr(RGB[1]).encode('latin1'))
-# time.sleep(0.01)
-# ser.write(chr(RGB[2]).encode('latin1'))
-# time.sleep(0.01)
+# Set up Wiz Lights
+async def setupLights():
+    if(WIZLIGHTS_IPS):
+        for i in range(len(WIZLIGHTS_IPS)):
+            LIGHTS[i] = pywizlight.wizlight(WIZLIGHTS_IPS[i])
+
+def lightHandler(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
 
 
+loop = asyncio.new_event_loop()
+thread = threading.Thread(target=lightHandler, args=(loop,), daemon=True)
+thread.start()
+
+asyncio.run_coroutine_threadsafe(setupLights(), loop)
+# Get Current State
+asyncio.run_coroutine_threadsafe(saveStates(LIGHTS), loop)
+# Main Loop.
 
 print("Entering main loop. Press Control-C to exit.")
 try:
@@ -126,11 +181,14 @@ try:
                         pass
                     elif message[1] in sustainedNotes:
                         # Not holding, but already been sustained, just add to held notes
-                        heldNotes[message[1]] = 127
+                        heldNotes[message[1]] = message[2]
+                        # However, update velocity // NEW
+                        sustainedNotes[message[1]] = message[2]
+                        sendNoteOn(message[1], message[2])
                     else:    
                         # Not holding. Add to held notes and sustained. Send serial.
-                        heldNotes[message[1]] = 127
-                        sustainedNotes[message[1]] = 127
+                        heldNotes[message[1]] = message[2]
+                        sustainedNotes[message[1]] = message[2]
                         sendNoteOn(message[1], message[2])
             elif(message[0] == 176 and message[1] == 64):
                 # Damper Pedal Control.. invert sustain and handle
@@ -146,13 +204,26 @@ try:
                     sustain = True
                     # Sustain is on. Subsequent notes should be sustained and currently held notes should be held in sustain
                     sustainedNotes = heldNotes.copy()
+                    # Check if there are notes being held and sustained or not
+            
+            if(len(sustainedNotes) == 0 and len(heldNotes) == 0):
+                for light in LIGHTS:
+                    future = asyncio.run_coroutine_threadsafe(updateLight(light[0], [0,0,0], 0), loop)
+            else:
+                allNotes = sustainedNotes | heldNotes
+                brightness = min(int(255 * (len(allNotes) / MAX_LIGHT_NUM_KEYS)), 255)
+                print(str(brightness))
+                for light in LIGHTS:
+                    future = asyncio.run_coroutine_threadsafe(updateLight(light[0], RGB, brightness), loop)
                         
                     
 
-        #time.sleep(0.001)
+
 except KeyboardInterrupt:
     print('')
 finally:
     print("Exit.")
     midiin.close_port()
+    endLoop = asyncio.get_event_loop()
+    endLoop.run_until_complete(updateLightStates(LIGHTS))
     del midiin
